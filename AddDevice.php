747541c7df23422f8616c32cc9202b92b116cab2
sandeep kumar <93798116+sandyvirat@users.noreply.github.com>
2022-04-08 20:08:44 +0200

    Add files via upload

---
+++ AddDevice.php
@@ -0,0 +1,24 @@
+<?php
+
+include('config.php');
+
+$ip = $_GET['ip'];
+$port = $_GET['port'];
+$community = $_GET['community'];
+$version = $_GET['version'];
+
+if(empty($ip) || empty($port) || empty($community) || empty($version)) {
+    echo "please provide proper input" ;   
+}
+
+else {
+
+    $db->exec("INSERT INTO info (IP,PORT,COMMUNITY,VERSION) VALUES ('$ip','$port','$community','$version')");
+        echo "\n";
+        echo "OK";
+    
+    }
+$db->close();
+
+?>
+© 2021 GitHub, Inc.
---
+++ removeDevices.php
@@ -0,0 +1,38 @@
+<?php
+include('config.php');
+
+if (empty($_GET)){
+	echo "none";
+    }
+else{
+
+	$ip = $_GET["ip"];
+	$port = $_GET['port'];
+	$community = $_GET['community'];
+	$version = $_GET['version'];
+
+	
+	$check = $db->query("SELECT * FROM info WHERE IP='$ip'");
+	while($i = $check->fetchArray(SQLITE3_ASSOC)){
+		
+			
+		if ($i['IP'] != $ip) {
+			echo 'No IP Found';
+		}
+		else{
+			$sql1 =<<<EOF
+			DELETE FROM info WHERE IP = '$ip';
+EOF;
+			$run1 = $db->exec($sql1);
+			
+			if(!$run1){
+				echo "FAIL";
+			}
+			else{
+				echo "OK removed";
+			}		}
+	}
+
+}
+
+$db->close();
